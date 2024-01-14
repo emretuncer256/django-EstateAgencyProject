a79270c9b788869ffec80b8a6a8f7b413913e710
Emre Tuncer <emretuncer.developer@gmail.com>
2024-01-14 21:34:00 +0300

    Modified: blog comments in admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,26 @@ from django.contrib import admin
 from .models import Blog, Comment, Category
 
 
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
+    inlines = [CommentsInline]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["blog", "name", "message", "is_parent", "created_at"]
+    search_fields = ["name", "message"]
+    list_filter = ["blog", "created_at"]
+
+    @admin.display(boolean=True)
+    def is_parent(self, obj):
+        return not obj.has_parent()
 
 
 @admin.register(Category)
